06b3303b5c9a09abbf962f72791abb244e026c79
shizhidi <shizhido@outlook.com>
2018-10-03 23:28:54 +0800

    breach

--- .history/src/components/HelloWorld_20181003232757.vue
+++ .history/src/components/HelloWorld_20181003232757.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App first tag change Content! create breach'
+      msg: 'Welcome to Your Vue.js App first tag change Content!teststst'
     }
   }
 }
